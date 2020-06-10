512182665c8f326c8dae51bbf69d5f30bb5b4d78
devagni98 <devagnihotrid@gmail.com>
2020-06-10 19:01:51 +0530

    assignment 1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 
 import './App.css';
 import Menu from './components/MenuComponent';
+
 import {DISHES} from './shared/dishes';
 
 class App extends Component{
